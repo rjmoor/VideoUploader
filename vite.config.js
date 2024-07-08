db64f6312abae3a31a7ff432a7e9179bd3bfd3c8
rjmoor <rj.aem.consulting@gmail.com>
2024-07-08 17:19:06 -0400

    Updates
    updated

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,11 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: "/VideoUploader",
   plugins: [react()],
-  
+  build: {
+    outDir: 'dist',
+    rollupOptions: {
+      input: '/index.html',
+    },
+  },
 });
